25b8cd3c597ecae2717d0f77d87e7a4e378dc361
gabrielamiranda31-boop <gabriela.mirandar31@gmail.com>
2025-10-07 17:19:28 -0600

    Clase

---
+++ Semana 4/clase 07_10.py
@@ -0,0 +1,19 @@
+nombre = "Alvin"
+palabra = "RECONOCER"
+otra_palabra = "palabra"
+
+""""print(len(nombre))
+      
+   print(nombre[2])
+   
+   print(nombre2:5])"""
+
+
+print(len(palabra))
+print(palabra[0: :2]) #consonante
+print(palabra[0: :3]) #vocal 
+print(palabra[: :3])                        # saltandome el principio
+print(palabra[::-1]) # salto alreves
+
+print(otra_palabra[::-1])
+
